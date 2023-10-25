29d4ac0b40ab33d165a3a3d9636a2ae19d69132e
evi1Husky <evi1husky0@gmail.com>
2023-10-25 15:46:02 +1000

    .

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,5 @@ const vite_1 = require("vite");
 const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
 // https://vitejs.dev/config/
 exports.default = (0, vite_1.defineConfig)({
-  base: "/DSAvisualizer/",
   plugins: [(0, plugin_react_1.default)()],
 });
